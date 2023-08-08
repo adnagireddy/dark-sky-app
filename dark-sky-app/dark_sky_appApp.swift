850cc0aad9510433cbea9af1e430a84feaef21e8
Ananya Nagireddy <ananya.d.nagireddy@gmail.com>
2023-08-07 21:29:43 -0400

    Initial Commit

---
+++ dark-sky-app/dark_sky_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  dark_sky_appApp.swift
+//  dark-sky-app
+//
+//  Created by Ananya Nagireddy on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct dark_sky_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
